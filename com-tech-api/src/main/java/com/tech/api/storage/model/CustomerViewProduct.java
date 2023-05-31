074524110e4b714cb44ceb0a24e9ff144c359820
PhongZo <lagiaphong.2001phong@gmail.com>
2023-05-31 23:04:38 +0700

    get data train

---
+++ com-tech-api/src/main/java/com/tech/api/storage/model/CustomerViewProduct.java
@@ -0,0 +1,26 @@
+package com.tech.api.storage.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = TablePrefix.PREFIX_TABLE + "customer_view")
+public class CustomerViewProduct {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    private Long timestamp;
+}
